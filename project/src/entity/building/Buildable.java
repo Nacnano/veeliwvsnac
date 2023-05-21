20429472ef8bd1a4344ec14f849b5b893e0adfe1
nac <chotpisit080916c@gmail.com>
2023-05-21 13:02:39 +0700

    add javadoc to interfaces and BaseBuilding

--- project/src/entity/building/Buildable.java
+++ project/src/entity/building/Buildable.java
@@ -1,8 +1,22 @@
 package entity.building;
 
+import entity.unit.BaseUnit;
+import game.Position;
 import game.Terrain;
 
+/**
+ * This interface defines methods for {@link BaseBuilding} that can 
+ * build on a {@link Terrain}
+ */
 public interface Buildable {
 
+	/**
+	 * This methods is called when {@link BaseUnit} is moving to specific
+	 * {@link Position}.
+	 * 
+	 * @param t The terrain which building will be building on
+	 * @return true If this building can build on the terrain 
+	 * otherwise false
+	 */
 	public boolean canBuildOn(Terrain t);
 }
--- project/src/entity/unit/Movable.java
+++ project/src/entity/unit/Movable.java
@@ -2,7 +2,18 @@ package entity.unit;
 
 import game.Position;
 
+/**
+ * This interface defines methods for {@link BaseUnit} that can move to another
+ * {@link Position}.
+ */
 public interface Movable {
 
+	/**
+	 * This methods is called when {@link BaseUnit} is moving to specific
+	 * {@link Position}.
+	 * 
+	 * @param p The position which entity will be moving to
+	 * 
+	 */
 	public void move(Position p);
 }
